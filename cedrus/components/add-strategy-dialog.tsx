3d896f1144bcae493d458cb8240075926f5d3af6
Bora <borabalos9@gmail.com>
2025-01-14 12:35:08 +0100

    style: Added labels to add strategy dialog and restructured the layout and gapping

--- cedrus/components/add-strategy-dialog.tsx
+++ cedrus/components/add-strategy-dialog.tsx
@@ -12,6 +12,7 @@ import {
     DialogFooter
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label"
 import { Plus } from "lucide-react";
 
 
@@ -41,15 +42,27 @@ export function AddStrategyDialog({ onAddStrategy }: AddStrategyDialogProps) {
                 </Button>
             </DialogTrigger>
 
-            <DialogContent>
+            <DialogContent className="sm:max-w-[425px]">
                 <DialogHeader>
-                    <DialogTitle>Ajouter une stratégie</DialogTitle>
+                    <DialogTitle className="font-semibold">Ajouter une stratégie</DialogTitle>
                 </DialogHeader>
                 
                 <form onSubmit={handleSubmit}>
-                    <Input placeholder="Nom de la stratégie" value={name} onChange={(e) => setName(e.target.value)} required />
-                    <Input type="number" placeholder="Année cible" value={targetYear} onChange={(e) => setTargetYear(e.target.value)} required />
-                    
+                    <div className="flex flex-col gap-4 pt-4 pb-8">
+                        <div className="flex flex-col gap-2">
+                            <Label htmlFor="name">
+                                Nom de la stratégie
+                            </Label>
+                            <Input placeholder="e.g. DPE B" value={name} onChange={(e) => setName(e.target.value)} required />
+                        </div>
+                        
+                        <div className="flex flex-col gap-2">
+                            <Label htmlFor="year">
+                                Année cible
+                            </Label>
+                            <Input type="number" placeholder="e.g. 2050" value={targetYear} onChange={(e) => setTargetYear(e.target.value)} required />
+                        </div>
+                    </div>
                     <DialogFooter>
                         <Button type="submit" variant="default">
                             Ajouter la stratégie
