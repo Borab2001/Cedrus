3fed09e5a6b0c5c5b8f22ce65c5b9a050de12e3d
Bora <borabalos9@gmail.com>
2025-01-14 11:46:32 +0100

    style: Table upper input and buttons styling

--- cedrus/app/page.tsx
+++ cedrus/app/page.tsx
@@ -1,12 +1,31 @@
 import { columns } from "@/components/columns";
 import { DataTable } from "@/components/data-table";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { ListFilter, Plus } from "lucide-react";
 
 export default function Home() {
 	return (
-		<div className="min-h-screen container p-8 font-[family-name:var(--font-geist-sans)]">
+		<div className="min-h-screen mx-auto container p-8 font-[family-name:var(--font-geist-sans)]">
 			<h1 className="text-2xl font-semibold">Stratégies</h1>
 			<p className="text-muted-foreground font-medium text-base">Voici un aperçu de toutes les stratégies</p>
 
+			<div className="flex items-center justify-between space-x-2 py-4">
+				<Input
+				placeholder="Recherche"
+				className="max-w-sm"
+				/>
+				<div className="flex items-center space-x-2">
+					<Button variant="outline">
+						<ListFilter />
+						Filtres
+					</Button>
+					<Button variant="default">
+						<Plus />
+						Ajouter
+					</Button>
+				</div>
+			</div>
 			<DataTable columns={columns} data={[]} />
 		</div>
 	);
